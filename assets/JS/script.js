20787256fc145f387174787c858c25b0223b1f11
Emily Carlisle <emilydeluna96@gmail.com>
2020-04-19 21:33:22 -0500

    added countup&ageverify

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,9 +2,34 @@ var giphyApiKey = "bkAxzVVfXtpuYo0AmrdqJO1Gp3erKjJE";
 
 
 
-$(document).ready(function () {
-    $.ageCheck({minAge: 21});
 
+$(document).ready(function () { 
+    $.ageCheck({minAge: 21});
+    startCount();
+
+    function startCount() {
+        var today = moment("2020-07-23");
+        var then = moment("2020-03-24");
+        var duration = moment.duration(today.diff(then));
+        var months = duration.asMonths();
+        var weeks = duration.asWeeks();
+        var days = duration.asDays();
+        var hours = duration.asHours();
+        
+        console.log(months, weeks, days, hours);
+        
+
+        var cMonths = (Math.trunc(months));
+        var cWeeks = (Math.trunc(weeks))
+        var cDays = (Math.trunc(days))
+        var cHours = (Math.trunc(hours))
+        console.log(cMonths, cWeeks, cDays, cHours);
+        $("#months").text(cMonths);
+        $("#weeks").text(cWeeks);
+        $("#days").text(cDays);
+        $("#hours").text(cHours);
+
+    }
 
 
     // Get Method using spirit name or ingredient 
@@ -111,4 +136,17 @@ $(document).ready(function () {
         }
 
     }
+
+   //function startCount() {
+   // var start = moment("{2020-03-24}", "DD-MM-YYYY");
+   // var today = moment();
+//
+   // today.diff(start, "days")
+//
+   //}
+
+
+
+
+
 })
